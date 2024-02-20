2e92dfd2f80bdb17ba9661a9ef77d8c4c70e14d7
= <=>
2024-02-19 21:19:56 -0300

    fetch api joker parms Programming jokers

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const contentJoker = document.getElementById('joker');
 
-const PATH_URL = 'https://v2.jokeapi.dev/joke/Any?type=single&amount=1';
+const PATH_URL = 'https://v2.jokeapi.dev/joke/Programming?type=single&amount=1';
 
 async function generatedJoker() {
   const data = await fetch(PATH_URL).then((e) => e.json());
-
+  console.log(data.joke);
   if(data) {
     contentJoker.innerText = data.joke;
   }
